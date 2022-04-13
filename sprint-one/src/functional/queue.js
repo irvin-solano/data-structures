d9dd9c5caf9accd7906905291642881121fe2fb9
Irvin Solano <irvin.solano.ny@gmail.com>
2022-04-13 17:05:25 -0400

    Completed_Functional Instantiation

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,25 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.dequeue = function() {
+    if(count - Object.keys(storage).length < 0){
+      return
+    }
+    var takeAway = storage[count - Object.keys(storage).length];
+    delete storage[count - Object.keys(storage).length];
+    return takeAway
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length
   };
 
   return someInstance;
