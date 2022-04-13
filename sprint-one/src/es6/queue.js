a1ed03457a95da0f236de76bff02e8dc58219335
Irvin Solano <irvin.solano.ny@gmail.com>
2022-04-13 19:58:44 -0400

    Complete_ES6 Instantiation Pattern

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,26 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
+  }
+
+  enqueue (value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  dequeue () {
+    if (this.count - Object.keys(this.storage).length < 0) {
+      return;
+    }
+    var takeAway = this.storage[this.count - Object.keys(this.storage).length];
+    delete this.storage[this.count - Object.keys(this.storage).length];
+    return takeAway;
+  }
+
+  size () {
+    return Object.keys(this.storage).length;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,20 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  push(value) {
+    this.storage[Object.keys(this.storage).length] = value;
+  }
+
+  pop() {
+    var takeAway = this.storage[Object.keys(this.storage).length - 1];
+    delete this.storage[Object.keys(this.storage).length - 1];
+    return takeAway;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
