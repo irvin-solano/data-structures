97a3f13a6dc9ced523c9dbbe7bdc17754a810ce5
Irvin Solano <irvin.solano.ny@gmail.com>
2022-04-13 19:47:15 -0400

    Complete_Pseudoclassical Instantiation Style

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,20 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.queue = {};
+  this.count = 0;
 };
 
+Queue.prototype.size = function () {
+  console.log(Object.keys(this.queue).length);
+  return Object.keys(this.queue).length;
+};
+
+Queue.prototype.enqueue = function (value) {
+  this.queue[this.count] = value;
+  this.count++;
+};
+Queue.prototype.dequeue = function () {
+  var takeAway = this.queue[this.count - Object.keys(this.queue).length];
+  delete this.queue[this.count - Object.keys(this.queue).length];
+  return takeAway;
+};
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,17 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.stack = {};
 };
 
+Stack.prototype.size = function () {
+  return Object.keys(this.stack).length;
+};
 
+Stack.prototype.push = function (value) {
+  this.stack[Object.keys(this.stack).length] = value;
+};
+
+Stack.prototype.pop = function () {
+  var takeAway = this.stack[Object.keys(this.stack).length - 1];
+  delete this.stack[Object.keys(this.stack).length - 1];
+  return takeAway;
+};
